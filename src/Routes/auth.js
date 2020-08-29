3763cf327cb7b42d759804b531ffe7b885de971a
YusupJunaedi <yusup.junaedi97@gmail.com>
2020-08-29 23:41:40 +0700

    change method login

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -4,6 +4,6 @@ const authController = require("../Controllers/auth");
 const authRouter = express.Router();
 
 authRouter.post("/register", authController.register);
-authRouter.get("/login", authController.login);
+authRouter.post("/login", authController.login);
 
 module.exports = authRouter;
